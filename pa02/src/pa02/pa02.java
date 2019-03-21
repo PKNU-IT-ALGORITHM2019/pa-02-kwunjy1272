66ac5aa446260ebf6438a68c0db53c3ccad60add
Kwon Jun Yeong <kwunjy1272@naver.com>
2019-03-21 22:38:46 +0900

    commit

--- pa02/src/pa02/pa02.java
+++ pa02/src/pa02/pa02.java
@@ -7,8 +7,7 @@ import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class Recur {
-
+public class pa02 {
 	static int[] path;
 	static int[] data;
 	static double sum = 0;
@@ -16,10 +15,8 @@ public class Recur {
 	static int n;
 	static int[] x;
 	static int[] y;
-
+	
 	public static void main(String[] args) throws FileNotFoundException {
-
-		// int[] path;
 		for (int i = 0; i < 7; i++) {
 			long start = System.currentTimeMillis();
 			Scanner fileScanner = new Scanner(new File("input" + i + ".txt"));
@@ -38,12 +35,10 @@ public class Recur {
 			Perm(1, 0);
 			System.out.println("#input" + i + "\nanswer : " + min_distance);
 			System.out.println(Arrays.toString(path));
-
 			long end = System.currentTimeMillis();
 			System.out.println("실행 시간 : " + (end - start) / 1000.0 + "\n-----------------------------");
 			fileScanner.close();
 		}
-
 	}
 
 	static double getDistance(int x, int y, int x1, int y1) {
@@ -54,18 +49,15 @@ public class Recur {
 		int tmp = data[index];
 		data[index] = data[change];
 		data[change] = tmp;
-
 	}
 
 	static void Perm(int s, double cur_dis) {
 
 		if (min_distance < cur_dis)
-			// System.out.println("min : " + Arrays.toString(data));
 			return;
 		else if (s == n) {
 			double tmp = cur_dis;
 			tmp += getDistance(x[data[0]], y[data[0]], x[data[n - 1]], y[data[n - 1]]);
-			// System.out.println("path : " + Arrays.toString(data));
 			if (min_distance > tmp) {
 				min_distance = tmp;
 				System.arraycopy(data, 0, path, 0, n);
@@ -81,6 +73,5 @@ public class Recur {
 			}
 			return;
 		}
-
 	}
 }
